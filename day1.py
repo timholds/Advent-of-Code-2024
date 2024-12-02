6f755eea7566dbe42d4ceb241d6752834515a91e
Tim Holdsworth <tholdsworth@sandiego.edu>
2024-12-02 00:19:02 -0800

    solved part2

--- day1.py
+++ day1.py
@@ -1,11 +1,64 @@
 import numpy as np
+from collections import defaultdict
 
+# Part 1:
 data = np.loadtxt('data1.txt', dtype=int)
 
 # we want to sort each list smallest to largest
 list1 = np.sort(data[:, 0])
 list2 = np.sort(data[:, 1])
 
+# dummy data
+# list1 = np.array([1, 2, 3, 4, 3])
+# list2 = np.array([1, 3, 3, 3, 8])
+
 # get sum of absolute pairwise distances
 sum_sorted_distances = np.sum(np.abs(np.subtract(list1, list2)))
 print(sum_sorted_distances)
+
+
+
+
+# Part 2: how often do items from list 1 occur in list 2
+intersection = np.intersect1d(list1, list2)
+print(intersection)
+num_overlapping = len(intersection)
+print(f'num_overlapping {num_overlapping} out of {len(list1)}')
+
+
+# every number not in the intersection will have a score of 0
+# for each num in intersection, we could do num * occur in list1 * occur list2
+s = []
+for unique_num in intersection:
+    occur_list1 = np.count_nonzero(list1 == unique_num)
+    occur_list2 = np.count_nonzero(list2 == unique_num)
+    score = unique_num * occur_list1 * occur_list2
+    print(unique_num, occur_list1, occur_list2, score)
+    s.append(score)
+
+print(sum(s))
+
+print('!!!!!!')
+
+# make a count dict of the unique numbers - {list1_num: occurences_in_list2}
+unique_nums = set(list(list1))
+occurences = {num: 0 for num in unique_nums}
+
+# occurences = {int(u): int(c) for u, c in zip(unique, count)}
+# print(occurences)
+scores = []
+
+for i, num in enumerate(list1):
+    if num not in occurences:
+        # TODO calculate how many times it showed up
+        count = np.count_nonzero(list2 == list1[i])
+        if count != 0:
+            print('!!!!')
+        occurences[num] = count
+    # else: # we already have the count for that one in the dict 
+    #     print('!!\n')
+    count = np.count_nonzero(list2 == list1[i])
+    scores.append(int(num * occurences[num]))
+
+# print(scores)
+print(np.sum(scores))
